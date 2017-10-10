13bdd0fe1b989f81e7348436b76f2c55f7116c11
Mateus Gabi - LNV/W10 <mateusgabimoreira@hotmail.com>
2017-10-10 09:42:25 -0400

    forcing error

--- src/app.test.js
+++ src/app.test.js
@@ -4,19 +4,6 @@ var request = require('request-promise');
 let PORT = process.env.PORT || 8080;
 let API_URI = 'http://localhost:' + PORT;
 
-// test('checking API PORT ', () => {
-
-//     expect(8080).toBe(PORT);
-
-// });
-
-// test('route /', () => {
-//     request(API_URI)
-//         .then((body) => {
-//             expect(body).toBeDefined();
-//         });
-// });
-
 test('forcing error', () => {
-    expect(false).toBe(false);
+    expect(true).toBe(false);
 });
